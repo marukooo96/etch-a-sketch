a441c97c93f94d293cdfdf1ed62d9fe8c804177d
Marco Vasile <marcova996@gmail.com>
2024-09-16 17:56:01 +0200

    Add css file

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const container = document.createElement('div');
 container.setAttribute('id', 'container');
 
+createGrid();
+
 
 
 function createGrid() {
     for (i = 0; i < 16; i++) {
         const div = document.createElement('div');
+        div.setAttribute('class', 'grid');
     }
 }
