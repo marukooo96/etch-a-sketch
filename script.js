e5e930a88ee0f1f62cbe84c9e0e28c7652ec5afb
Marco Vasile <marcova996@gmail.com>
2024-09-16 17:27:38 +0200

    Create main container

---
+++ script.js
@@ -0,0 +1,10 @@
+const container = document.createElement('div');
+container.setAttribute('id', 'container');
+
+
+
+function createGrid() {
+    for (i = 0; i < 16; i++) {
+        const div = document.createElement('div');
+    }
+}
