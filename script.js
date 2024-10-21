667d07bf9d1906b096c94071a946a30c5acec7c4
Marco Vasile <marcova996@gmail.com>
2024-10-21 17:56:54 +0200

    Write a function to create a 16x16 square grid

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+// const container = document.getElementById('container');
+// const div = document.createElement('div');
+// container.appendChild(div).className = 'grid';
+
 const container = document.getElementById('container');
-const div = document.createElement('div');
-container.appendChild(div).className = 'grid';
+createGrid();
+
+
+
+function createGrid() {
+    for (i = 1; i < 17; i++) {
+        const row = document.createElement('div');
+        row.id = i;
+        container.appendChild(row).className = 'row';
+        for (j = 1; j < 17; j++) {
+            const div = document.createElement('div');
+            row.appendChild(div).className = 'grid';
+        }
+    }
+}
