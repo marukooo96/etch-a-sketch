7f96b41e2f561bf5ee7dc259a68cce09380b00d3
Marco Vasile <marcova996@gmail.com>
2024-10-28 21:33:24 +0100

    Create function to create a custom grid

--- script.js
+++ script.js
@@ -12,14 +12,35 @@ function createGrid() {
     for (i = 1; i < 17; i++) {
         const row = document.createElement('div');
         row.id = i;
-        container.appendChild(row).className = 'row';
+        grid.appendChild(row).className = 'row';
         for (j = 1; j < 17; j++) {
             const div = document.createElement('div');
-            row.appendChild(div).className = 'grid';
+            div.addEventListener('mouseover', () => {
+                div.style.backgroundColor = 'black';
+            });
+            row.appendChild(div).className = 'square';
         }
     }
 }
 
 function getNumber() {
-    let number = prompt("Select the mumber of squares for your grid");
+    let n = prompt("Select the mumber of squares for your grid");
+
+    document.getElementById('grid').outerHTML = '';
+
+    const grid = document.createElement('div');
+    container.appendChild(grid).id = 'grid' ;
+
+    for (j = 1; j <= n; j++) {
+        const row = document.createElement('div');
+        row.id = j;
+        grid.appendChild(row).className = 'row';
+        for (k = 1; k <= n; k++) {
+            const div = document.createElement('div');
+            div.addEventListener('mouseover', () => {
+                div.style.backgroundColor = 'black';
+            });
+            row.appendChild(div).className = 'square';
+        }
+    }
 }
